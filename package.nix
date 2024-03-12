e957db5ec9aed061867b8e857d48dbb074945e4d
Meili C <sweetbabyalaska@gmail.com>
2024-03-11 17:25:30 -0800

    update nix files

--- package.nix
+++ package.nix
@@ -1,42 +1,45 @@
-{ lib, fetchFromGitHub, buildGoModule, upx }:
-
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+  upx,
+}:
 buildGoModule rec {
-    pname = "tget";
-    version = "0.1";
+  pname = "tget";
+  version = "0.1";
 
-    src = fetchFromGitHub {
-      owner = "sweetbbak";
-      repo = "tget";
-      rev = "959e5f4c89156b789e35a92438735004716d12a9";
-      hash = "sha256-3pf5woxqUf7RfX32P21UwOBpsW1i6nkYdD6We0YjdFQ=";
-    };
+  src = fetchFromGitHub {
+    owner = "sweetbbak";
+    repo = "tget";
+    rev = "3643ce8f52696e4e476d7a22e59b962b90fed963";
+    hash = "sha256-UvtAEAQkpCaKtty+URw+feNeE+F49BWMD/rphHSqoi8=";
+  };
 
-    # vendorHash = lib.fakeHash;
-    vendorHash = "sha256-74++inwJPbpjPrK5Xn66t+s50wbA2H1RgCxrS7DVJiA=";
+  # vendorHash = lib.fakeHash;
+  vendorHash = "sha256-74++inwJPbpjPrK5Xn66t+s50wbA2H1RgCxrS7DVJiA=";
 
-    CGO_ENABLED = 0;
-    ldflags = [ "-s" "-w" ];
-    # buildInputs = [ "upx" ];
+  CGO_ENABLED = 0;
+  ldflags = ["-s" "-w"];
 
-    tags = [ "torrent" "bittorrent" "anime" ];
-    proxyVendor = true;
+  tags = ["torrent" "bittorrent" "anime"];
+  proxyVendor = true;
 
-    buildPhase = ''
-        go mod vendor
-        go build
-    '';
+  buildPhase = ''
+    go mod vendor
+    go build
+  '';
 
-    installPhase = ''
-        ${upx}/bin/upx -9 tget
-        mkdir -p $out/bin
-        mv tget $out/bin
-    '';
+  installPhase = ''
+    ${upx}/bin/upx -9 tget
+    mkdir -p $out/bin
+    mv tget $out/bin
+  '';
 
-    meta = with lib; {
-        homepage = "https://github.com/sweetbbak/tget";
-        description = "wget but for torrents";
-        license = licenses.mit;
-        maintainers = with maintainers; [ sweetbbak ];
-        mainProgram = "tget";
-    };
+  meta = with lib; {
+    homepage = "https://github.com/sweetbbak/tget";
+    description = "wget but for torrents";
+    license = licenses.mit;
+    maintainers = with maintainers; [sweetbbak];
+    mainProgram = "tget";
+  };
 }
