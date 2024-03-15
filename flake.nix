b256720812da985ed6b9499b5812b6379dcaf0ff
Meili C <sweetbabyalaska@gmail.com>
2024-03-14 16:57:37 -0800

    add desktop file

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-  outputs =
-    { nixpkgs, ... }:
-    {
-      packages.x86_64-linux =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in
-        {
-          default = pkgs.callPackage ./package.nix { };
-        };
+  outputs = {nixpkgs, ...}: {
+    packages.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      default = pkgs.callPackage ./nix/package.nix {};
     };
+  };
 }
